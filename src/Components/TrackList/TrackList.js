7306658bbb5e7bea0a3877ce8b822b6068587152
Courtland <courtland.griffin@sharp.com>
2020-07-28 11:30:17 -0700

    Passing searchResults to app components

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,13 +6,13 @@ import Track from '../Track/Track';
 class TrackList extends React.Component {
   render() {
     return (
-      <div className="TrackList">
-        {
-          this.props.tracks.map( track => {
-          return <Track track={track} key={track.id} />
-            }
-          )
-        }
+      <div className="TrackList" >
+          {
+            this.props.tracks.map(track => {
+              return <Track track={track}
+                key={track.id} />
+              })
+          }
       </div>
     )
   }
