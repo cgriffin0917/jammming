b6fd24ed7dad3fc3d050fdf573f9fb18fbc1101c
Courtland <courtland.griffin@sharp.com>
2020-08-04 13:35:23 -0700

    Completed_steps83

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,10 @@ import Playlist from '../Playlist/Playlist';
       }
       this.addTrack = this.addTrack.bind(this);
       this.removeTrack = this.removeTrack.bind(this);
+      this.updatePlaylistName = this.updatePlaylistName.bind(this);
+      this.savePlaylist = this.savePlaylist.bind(this);
+      this.search = this.search.bind(this);
+
     }
 
  /* Review step 41 if this doesn't work */
@@ -35,6 +39,11 @@ import Playlist from '../Playlist/Playlist';
         }
       }
 
+    updatePlaylistName(name) {
+      this.setState({playlistName: name})
+
+      }
+
     removeTrack(track) {
       let tracks = this.state.playlistTracks;
       if (tracks.filter(currentTrack => currentTrack.id !== track.id)) {
@@ -43,16 +52,24 @@ import Playlist from '../Playlist/Playlist';
       }
     }
 
+    savePlaylist() {
+      const trackURIs = this.state.playlistTracks.map(track => track.uri)
+    }
+
+    search(Term) {
+      console.log(Term);
+    }
+
     render() {
       return (
         <div>
           <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
-            <SearchBar />
+            <SearchBar onSearch={this.search} />
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
               <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
-              onRemove={this.removeTrack} />
+              onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
             </div>
           </div>
         </div>
