88982f1673205f823b895594a7d4a3c563d4e5f7
Courtland <courtland.griffin@sharp.com>
2020-08-08 13:15:00 -0700

    completed spotifyapi

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,22 +3,15 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
   class App extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        searchResults: [
-          {name: 'name1', artist: 'artist1', album: 'album', id: 1},
-          {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-          {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
-        ],
+        searchResults: [],
         playlistName: 'Basketball',
-        playlistTracks: [
-          {name: 'PlaylistName1', artist: 'PlaylistArtist1', album: 'PlaylistAlbum1', id: 4},
-          {name: 'PlaylistName2', artist: 'PlaylistArtist2', album: 'PlaylistAlbum2', id: 5},
-          {name: 'PlaylistName3', artist: 'PlaylistArtist3', album: 'PlaylistAlbum3', id: 6}
-        ]
+        playlistTracks: []
       }
       this.addTrack = this.addTrack.bind(this);
       this.removeTrack = this.removeTrack.bind(this);
@@ -53,11 +46,19 @@ import Playlist from '../Playlist/Playlist';
     }
 
     savePlaylist() {
-      const trackURIs = this.state.playlistTracks.map(track => track.uri)
+      const trackUris = this.state.playlistTracks.map(track => track.uri)
+      Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+        this.setState({
+          playlistName: 'New Playlist',
+          playlistTracks: []
+        })
+      })
     }
 
-    search(Term) {
-      console.log(Term);
+    search(term) {
+      Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+      })
     }
 
     render() {
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Track.css';
-import Playlist from '../Playlist/Playlist';
+
 
 class Track extends React.Component {
 
